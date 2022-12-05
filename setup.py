41aa017ef583550a30d9fa50da5b10102a33da89
Veillax135 <arp.putland@gmail.com>
2022-12-05 09:10:23 -0700

    Update README.md & Create setup.py
    Signed-off-by: Veillax <arp.putland@gmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python-multiproject',
+    version='1.0.0',
+    packages=['pytz'],
+    url='https://github.com/Veillax135/python-multiproject',
+    license='CC-NC-ND 4.0',
+    author='Veillax135',
+    author_email='arp.putland@gmail.com',
+    description='multiproject scripted in Python'
+)
